1a6009537c79c40b4a2b0716fa3b3d927d5b9b6c
Maksat Duishenaliev <maksatovy@gmail.com>
2019-02-08 11:08:40 +0600

    Preloader added, works well

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Preloader.css'
+
+const Preloader = () => {
+    return (
+        <div className='Spinner'>Loading...</div>
+    );
+};
+
+export default Preloader;
